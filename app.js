6fc6431a3070c72b5a4563b5b427c2cbfaa542f0
Dano <danielaherniv@gmail.com>
2019-10-23 22:37:28 -0400

    added .gitignore

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 //Middlewares
-
+// app.use('/posts', () => {
+//     console.log('This is a middleware running')
+// });
 
 //Routes
 
@@ -14,6 +17,11 @@ app.get('/', (req, res) => {
 app.get('/posts', (req, res) => {
     res.send('We are on posts');
 });
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true
+}, () => console.log('connected to DB!'));
 
 //How do we start listening to the server
 app.listen(3000);
